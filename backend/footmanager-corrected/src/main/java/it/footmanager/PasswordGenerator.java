cc42e37566f8776c2b90a79465b1154f9bb1d8eb
manilaexe <manila.mingozzi@gmail.com>
2026-07-17 09:45:46 +0200

    -progetto che DOVREBBE FUNZIONARE

--- backend/footmanager-corrected/src/main/java/it/footmanager/PasswordGenerator.java
+++ backend/footmanager-corrected/src/main/java/it/footmanager/PasswordGenerator.java
@@ -5,6 +5,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordGenerator {
     public static void main(String[] args) {
         var encoder = new BCryptPasswordEncoder(12);
-        System.out.println(encoder.encode("ekhator"));
+        System.out.println(encoder.encode("celik"));
     }
 }
